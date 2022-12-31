72c0951a1f1da3381916c1e3efa5788438f0e2e4
codenoschool <codenoschool@gmail.com>
2022-12-31 01:32:16 -0600

    JWT auth implemented; requirements updated.
    The app's code has received several changes in order to implement
    authentication by using JWTAuth. Here are the most important changes:
    
    * A new class User was created as a DB Model.
    * A new class UserSchema was created as a schema for serializing and
    loading user objects.
    * The Framework and FrameworkSchema classes were modified since now
    there's a relationship between frameworks and user objects which is
    a foreign key called "author_id".
    * A new POST API Endpoint was created ("/api/users") which will be used
      as an endpoint to sign up new users. Some validations are made in this
    endpoint as well such as throwing errors when an user already exists.
    * An application secret key was defined and it *MUST* be changed before
    using it in a production environment.
    * A new Endpoint was created ("/login") which is not necessarily
    defined under the REST API Endpoints design. It'll be used for loggin-in
    users and if there's success then a new JWT will be issued. Such JWT
    will contain the user's id and username together with an expiration
    timestamp that'll be used wherever the JWT needs validation. The JWT is
    signed with the value of the application's secret key.
    
    Now JWT Authentication is required for some endpoints:
    
    * create_framework
    * update_framework
    * destroy_framewok
    
    Because of that, now some new validations are made such as being able to
    decode the JWT's contents successfully. Such validation will need code
    refactorization under the DRY principle.
    
    Changes for the file requirements.historical_refrence:
    
    * Now there are references for the new package installed: pyjwt.

--- app.py
+++ app.py
@@ -10,17 +10,26 @@ from flask_sqlalchemy import SQLAlchemy
 
 from marshmallow import Schema, fields
 
+from werkzeug.security import generate_password_hash, \
+        check_password_hash
+from datetime import datetime, timezone, timedelta
+
 import json
 
 import os
+
+import jwt
 
 BASE_DIR = os.path.dirname(__file__)
 DB_FILE = os.path.join(BASE_DIR, "development.sqlite3")
 SQLITE_DB_URI = "sqlite:///" + DB_FILE
 
 app = Flask(__name__)
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLITE_DB_URI
+
+app.config["SECRET_KEY"] = "sk"
 
 db = SQLAlchemy(app)
 
@@ -31,18 +40,57 @@ class Framework(db.Model):
 
     name = db.Column(db.String(50), unique=True)
 
-    def __init__(self, name):
+    author_id = db.Column(db.ForeignKey("users.id"), nullable=False)
+
+    def __init__(self, name, author_id):
         self.name = name
+        self.author_id = author_id
 
     def __repr__(self):
 
         return f"Framework(id={self.id}, name={self.name})"
 
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    username = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(88), nullable=True)
+
+    def hash_password(self, password):
+
+        return generate_password_hash(
+                password,
+                method="sha256"
+                )
+
+    def check_password(self, password):
+
+        return check_password_hash(
+                self.password,
+                password)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = self.hash_password(password)
+
+    def __repr__(self):
+
+        return f"User(id={self.id}, username={self.username})"
+
+
 class FrameworkSchema(Schema):
     id = fields.Int()
     name = fields.Str()
+    author_id = fields.Int()
+
+class UserSchema(Schema):
+    id = fields.Int()
+    username = fields.Str()
 
 framework_schema = FrameworkSchema()
+user_schema = UserSchema()
 
 @app.route("/api/frameworks", methods=["GET"])
 def get_frameworks():
@@ -74,6 +122,34 @@ def get_framework(id):
 
 @app.route("/api/frameworks", methods=["POST"])
 def create_framework():
+    try:
+        decoded_jwt = jwt.decode(
+                request.headers["JWT"],
+                app.config["SECRET_KEY"],
+                algorithms=["HS256"]
+                )
+
+        user = User.query.get(decoded_jwt["user_id"])
+
+        if not user:
+
+            return jsonify({
+                "message": "Data not found",
+                "status_code": 404
+                }), 404
+    except jwt.exceptions.ExpiredSignatureError:
+
+        return {
+                "message": "Login time-out",
+                "status_code": 440
+                }, 440
+    except:
+
+        return {
+                "message": "Forbidden",
+                "status_code": 403
+                }, 403
+
     framework = Framework.query.filter_by(
             name=request.json["name"]).first()
 
@@ -85,21 +161,47 @@ def create_framework():
                 }, 409
 
     new_framework = Framework(
-            name=request.json["name"]
+            name=request.json["name"],
+            author_id=user.id
             )
 
     db.session.add(new_framework)
     db.session.commit()
 
-    new_framework_dict = {
-            "id": new_framework.id,
-            "name": new_framework.name
-            }
+    new_framework_dict = framework_schema.dump(new_framework)
 
     return new_framework_dict
 
 @app.route("/api/frameworks/<int:id>", methods=["PUT"])
 def update_framework(id):
+    try:
+        decoded_jwt = jwt.decode(
+                request.headers["JWT"],
+                app.config["SECRET_KEY"],
+                algorithms=["HS256"]
+                )
+
+        user = User.query.get(decoded_jwt["user_id"])
+
+        if not user:
+
+            return jsonify({
+                "message": "Data not found",
+                "status_code": 404
+                }), 404
+    except jwt.exceptions.ExpiredSignatureError:
+
+        return {
+                "message": "Login time-out",
+                "status_code": 440
+                }, 440
+    except:
+
+        return {
+                "message": "Forbidden",
+                "status_code": 403
+                }, 403
+
     framework = Framework.query.get(id)
 
     if not framework:
@@ -119,6 +221,34 @@ def update_framework(id):
     
 @app.route("/api/frameworks/<int:id>", methods=["DELETE"])
 def destroy_framework(id):
+    try:
+        decoded_jwt = jwt.decode(
+                request.headers["JWT"],
+                app.config["SECRET_KEY"],
+                algorithms=["HS256"]
+                )
+
+        user = User.query.get(decoded_jwt["user_id"])
+
+        if not user:
+
+            return jsonify({
+                "message": "Data not found",
+                "status_code": 404
+                }), 404
+    except jwt.exceptions.ExpiredSignatureError:
+
+        return {
+                "message": "Login time-out",
+                "status_code": 440
+                }, 440
+    except:
+
+        return {
+                "message": "Forbidden",
+                "status_code": 403
+                }, 403
+
     framework = Framework.query.get(id)
 
     if framework:
@@ -126,6 +256,59 @@ def destroy_framework(id):
         db.session.commit()
 
         return {}, 204
+    else:
+
+        return jsonify({
+            "message": "Data not found",
+            "status_code": 404
+            }), 404
+
+@app.route("/api/users", methods=["POST"])
+def create_user():
+    user = User.query.filter_by(
+            username=request.json["username"]).first()
+
+    if user:
+
+        return {
+                "message": "Conflict",
+                "status_code": 409
+                }, 409
+
+    new_user = User(
+            username=request.json["username"],
+            password=request.json["password"]
+            )
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    new_user_json = user_schema.dump(new_user)
+
+    return new_user_json
+
+@app.route("/login", methods=["POST"])
+def login():
+    user = User.query.filter_by(
+            username=request.json["username"]).first()
+
+    if user and user.check_password(
+            request.json["password"]):
+        encoded_jwt = jwt.encode(
+                {
+                    "iss": user.username,
+                    "user_id": user.id,
+                    "exp": datetime.now(
+                        tz=timezone.utc
+                        ) + timedelta(minutes=2)
+                    },
+                app.config["SECRET_KEY"],
+                algorithm="HS256"
+                )
+                    
+        return jsonify({
+            "JWT": encoded_jwt
+            })
     else:
 
         return jsonify({
