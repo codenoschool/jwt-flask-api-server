3cd2f5c32b9a2e94777a0fb0677805ca48ff8005
codenoschool <codenoschool@gmail.com>
2022-12-30 20:54:09 -0600

    Creating the first api endpoint.
    The first api endpoint of the flask app will be: /api/frameworks and
    it will server a list of objects and such objects are entities, in this
    case, each entity represents a Framework (a piece of software).
    
    We will dump our (python) list of Frameworks into a JSON that we will
    serve at our application endpoint and we'll do it a in way in which
    we'll fully understand the proccess of how it's done.

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
 from flask import Flask
+from flask import make_response
+
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
+frameworks = [
+        {
+            "id": 1,
+            "name": "Flask"
+            },
+        {
+            "id": 2,
+            "name": "Angular"
+            },
+        {
+            "id": 3,
+            "name": "Laravel"
+            },
+        ]
 
-    return "Hello World!"
+@app.route("/api/frameworks")
+def index():
+    frameworks_json = json.dumps(
+            frameworks,
+            indent=4,
+            separators=(", ", ": ")
+            )
+
+    response = make_response(
+            frameworks_json,
+            200)
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
